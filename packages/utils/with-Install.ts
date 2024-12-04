8b45344c46f19d21cd4786e5bf9437e86a5cc076
john <deweizhong@outlook.com>
2024-12-04 20:49:00 +0800

    fix: 修复withInstall类型报错

--- packages/utils/with-Install.ts
+++ packages/utils/with-Install.ts
@@ -1,10 +1,10 @@
-import { Plugin } from 'vue';
+import { App, Plugin } from 'vue';
 
 export type SFCWithInstall<T> = T & Plugin;
 export function withInstall<T>(comp: T) {
-	(comp as SFCWithInstall<T>).install = function (app) {
+	(comp as SFCWithInstall<T>).install = function (app: App) {
 		const { name } = comp as unknown as { name: string };
-		app.component(name, comp); // 将组件注册成全局的
+		app.component(name, comp as SFCWithInstall<T>); // 将组件注册成全局的
 	};
 	return comp as SFCWithInstall<T>;
 }
